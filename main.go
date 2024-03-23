a926a657f30fa502ca4b6f4440580f9b3de8f9c6
RageNeko26 <rneko2006@gmail.com>
2024-03-24 05:51:21 +0700

    feat: Add example of Put Item Oject

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"log"
+
+	"os"
 	"time"
 
 	"github.com/minio/minio-go/v7"
@@ -10,7 +12,7 @@ import (
 )
 
 const (
-	ENDPOINT   = "localhost:9000"
+	ENDPOINT   = "127.0.0.1:9000"
 	ACCESS_KEY = "HE0BGWT4CTYQ90AQ9A8D"
 	SECRET     = "vk7J+Bat6I6NoQi17LNiEe4zOUyv7N+6d7xYJSgD"
 )
@@ -26,7 +28,8 @@ func main() {
 	}
 
 	//CreateBucket(client, "test-bucket")
-	ListBucket(client)
+	//ListBucket(client)
+	UploadItem(client, "bijou-lagi")
 }
 
 func ListBucket(client *minio.Client) {
@@ -54,4 +57,42 @@ func CreateBucket(client *minio.Client, name string) {
 	}
 
 	log.Println("Bucket has been created!")
+}
+
+func UploadItem(client *minio.Client, name string) {
+	file, err := os.Open("bijou.jpeg")
+
+	if err != nil {
+		log.Fatal("Failed to open file because error:", err)
+	}
+
+	stat, err := file.Stat()
+
+	if err != nil {
+		log.Fatal("Failed to get information of file:", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+
+	upload, err := client.PutObject(
+		ctx,
+		"test-bucket",
+		name,
+		file,
+		stat.Size(),
+		minio.PutObjectOptions{
+			ContentType: "image/jpeg",
+		},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to upload file into Storage Object:", err)
+	}
+
+	signedURL := "http://" + ENDPOINT + "/test-bucket/" + name
+
+	log.Println("File has been uploaded:", upload.Bucket)
+	log.Println("Signed URL: ", signedURL)
+
 }
